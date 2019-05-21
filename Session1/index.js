e98f3bdb55934d71c8c1a9c355c9b1b3b1778a37
dxtrann <anhdungtran.dxtn@gmail.com>
2019-05-21 19:26:25 +0700

    Tran Anh Dung

---
+++ Session1/index.js
@@ -0,0 +1,62 @@
+// console.log("hello world!!!");
+
+let name = "Dung"
+
+name = "Trung"
+
+console.log("hello", name)
+
+
+let age = prompt("enter your age: ");
+console.log(age);
+
+let born = prompt("enter born: ");
+let current_year = prompt("enter current year: ");
+console.log(current_year - born);
+
+let length = prompt("enter length: ")
+let width = prompt("enter width: ")
+console.log( (Number(length) + Number(width))*2 )
+
+for (let i =0 ; i < 100;i++){
+    fd(90)
+    rt(90)
+
+    fd(90)
+    rt(90)
+
+    fd(90)
+    rt(90)
+
+    fd(90)
+    rt(90)
+
+    rt(30)
+}
+
+
+for(let i = 1; i< 10; i++){
+    console.log(i)
+}
+
+for (let i = 9; i>0; i--){
+    console.log(i)
+}
+
+for (let i = 2; i< 50; i = i+2){
+    // if(i % 2 == 0){
+    // console.log(i)
+}
+
+
+
+
+let n = 10;
+
+let count = 0
+for (let i = 1; i< n + 1; i++){
+    count = count + i
+    // console.log(count)
+}
+console.log(count)
+
