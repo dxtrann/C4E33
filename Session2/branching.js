26a8986d0cd089e76f4d823fda9eca46aeee4a9c
dxtrann <anhdungtran.dxtn@gmail.com>
2019-05-24 13:41:06 +0700

    hello

---
+++ Session2/branching.js
@@ -0,0 +1,118 @@
+// // let birth_year = prompt("Enter your birth year: ")
+// // let age = 2019 - birth_year
+
+// // if (age <10) {
+// //     console.log("Baby")
+// // } 
+// // else if (age<15){
+// //     console.log("Young Buffalo")
+// // }
+// // else if (age<18){
+// //     console.log("Teenager")
+// // }
+// // else {
+// //     console.log("Adult")
+// // }
+
+
+// // TQ: if(condition equation){
+// //     Command
+// // } else {
+// //     console.log("Adult")
+// // }
+// // Command can only be executed when condition equation is correct
+// // Else: other situations
+// // only has ONE if and ONE else
+// // can have many else if 
+
+// // 1. Create a random number from 1 to 100
+// // var n = random
+// // if n from 0 to 30 sunny
+// // 31-70 rainy
+// // 71-100 cloudy
+
+// // var min=0; 
+// // var max=100;  
+// // var random =Math.floor(Math.random() * 100); 
+// //     document.write("Random Number Generated : " + random ); 
+// //     console.log(random)
+// // if (0 < random && random <= 30){
+// //     console.log("Sunny")
+// // }
+// // else if (31 < random && random <= 70){
+// //     console.log("Rainy")
+// // }
+// // else{
+// //     console.log("Cloudy")
+// // }
+
+// // *Note:
+// // Math.floor() round Number
+// // Math.random() create a random Number
+
+// // 2. Solve ax2 + bx + c = 0
+
+// // let a = prompt("Enter a: ")
+// // let b = prompt("Enter b: ")
+// // let c = prompt("Enter c: ")
+// // delta = Number (b*b - 4*a*c)
+
+// // if (delta < 0){
+// //     console.log("No value available")
+// // }
+// // else if (delta === 0){
+// //     x = Number (-b/(2*a))
+// //     console.log("Double value x =", x)
+// // }
+// // else {
+// //     x1 = Number((-b - Number(delta) ** 0.5) /Number((2*a)))
+// //     x2 = Number((-b + Number(delta) ** 0.5) /Number((2*a)))
+// //     console.log("2 separated values")
+// //     console.log("x1 = ", x1)
+// //     console.log("x2 = ", x2)
+// // }
+
+// // OR cpnsole.log('Double value x = ${x}')
+
+// // 3. So chan tu 1 den 100
+
+// // for (let i = 1; i<=100; i = i++) {
+// //     if (i % 2 == 0){
+// //     console.log(i)
+// // }
+// // }
+
+// // 4. Let user put in username
+// // if username correct let them put in password
+// // if pw correct console welcome 
+// // if pw wrong console wrong pw
+// // if wrong alert not found
+
+// let usn = prompt("Enter your username: ")
+// if (usn === "C4E33"){ 
+//     let pw = prompt("Enter your passwords: ")
+//     if (pw === "codethechange"){
+//         console.log("Welcome!")
+//     }
+//     else{
+//         console.log("Wrong Passwords")
+//     }
+//     }
+// else{ 
+//     console.log("Not found!")
+// }
+
+// Bai chua
+// let username = "c4e33"
+// let password = "codethechange"
+
+// let usernameLogin = prompt("Username?")
+// if (usernameLogin === username){
+//     let passwordLogin = prompt("Password?")
+//     if (passwordLogin === password){
+//         console.log(`Welcome ${usernameLogin}`)
+//     }
+//     else {
+//         console.log("Wrong PW")
+//     }
+// }
---
+++ Session2/loop.js
@@ -0,0 +1,53 @@
+// for (let i = 0; i < 5; i++){
+//     console.log(i);
+//     console.log("hello");
+// }
+
+// 1. Create a sequence [3;100]
+// 2. Create a sequence [3;6;9;...;99]
+// 3. Create a sequence [100;99...3]
+// 4. Create a sequence [100;97;94;91]
+
+// 1. 
+// for (let i = 3; i <= 100; i++){
+//     console.log(i)
+// }
+
+// // 2.
+// // for (let i = 3; i<=100; i=i+3){
+// //     console.log(i)
+// // }
+
+// // 3.
+// // for (let i = 100; i>= 3; i--){
+// //     console.log(i)
+// // }
+
+// // 4.
+// for (let i = 100; i>= 3; i=i-3){
+//     console.log(i)
+// }
+
+// 5. let user insert n 
+// console sum 1-n 
+
+// let n = Number (prompt("Enter n: "));
+// let result = 0
+// for (let i = 1; i <= n; i++){
+//     result = result + i  
+// }
+// console.log(result)
+
+// 6. Draw a maze
+// clear()
+// for (i=0; i<200; i=i+2){
+//   rt(90)
+//   fd(5+i)
+// }
+
+
+
+
+
+
+
